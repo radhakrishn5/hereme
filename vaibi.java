03eecc0d7184037b7aba8beed72b2e5ca2987c37
vaibhavi656 <vaibhaviangadi107@gmail.com>
2025-11-25 21:35:03 +0530

    Create vaibi.java

---
+++ vaibi.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n = 10;
+
+        if (n % 2 == 0) {
+            System.out.println(n + " is Even");
+        } else {
+            System.out.println(n + " is Odd");
+        }
+    }
+}
